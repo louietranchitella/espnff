a1547938340cfc1b175ff0adfe68654bb0eafd3c
louietranchitella <louie.tranchitella@gmail.com>
2023-10-24 18:32:02 -0400

    first commit

---
+++ grabber.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+from datetime import date
+
+league_id = 1570607
+start_year = 2018
+last_year = date.today().year
+
+if (date.today().month < 9):
+    last_year -= 1
+
+data = dict()
+
+for year in range(start_year, last_year, 1):
+    url = url = "https://fantasy.espn.com/apis/v3/games/ffl/leagueHistory/" + str(league_id) + "?seasonId=" + str(year)
+
+    r = requests.get(url,
+                     cookies={"swid": "{8095DE7B-520D-4619-BE38-33293C2BD1B4}",
+                              "espn_s2": "AEBiCbsmWjb1zWWG0%2B1DDj%2FxqAlV2eHcnYlKPJUA9f0WkcN9qmtyFeubqJDGH0EajqFWWjtUp5AWWtnBRhheukByJGEW0eJUfdHzlMMnNjplc7rXnXngO2DrT0P6r5sxJHOUmpvKz9wmbXDTBSWZkAHxZPtA1skkQMFGOqdxVt7GOkdV0xNj%2B5sdeeCQrKVS4sElWaIbhE13Piyqj%2FY5P7kguXN2QaOn6m5cdRytl97JJPdyelUXTEcWy7RHUeFJlhde%2Fx1ilfGJMT86WAKJIO6ef6paM72R7jYRyLA3uye0twXPm5qae2zjh3z18b6juvg%3D"},
+                     params={"view": "mBoxscore"})
+
+    json_data = r.json()[0]
+
+    json_formatted_str = json.dumps(json_data, indent=2)
+
+    data[str(json_data["seasonId"])] = json_data
+
+print(json.dumps(data))
+
+for year in data:
+    for team in data[year]["teams"]:
+        if "{8095DE7B-520D-4619-BE38-33293C2BD1B4}" in team["owners"]:
+            print(team)
+            continue
