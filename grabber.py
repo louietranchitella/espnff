dc2868798128977bb0c7bd75e4ea9d0b88fa2f4c
louietranchitella <louie.tranchitella@gmail.com>
2023-10-30 09:16:29 -0400

    condense request call

--- grabber.py
+++ grabber.py
@@ -7,6 +7,9 @@ from datetime import date
 league_id = 1570607
 start_year = 2018
 last_year = date.today().year
+cookies={"swid": "{8095DE7B-520D-4619-BE38-33293C2BD1B4}",
+         "espn_s2": "AEBiCbsmWjb1zWWG0%2B1DDj%2FxqAlV2eHcnYlKPJUA9f0WkcN9qmtyFeubqJDGH0EajqFWWjtUp5AWWtnBRhheukByJGEW0eJUfdHzlMMnNjplc7rXnXngO2DrT0P6r5sxJHOUmpvKz9wmbXDTBSWZkAHxZPtA1skkQMFGOqdxVt7GOkdV0xNj%2B5sdeeCQrKVS4sElWaIbhE13Piyqj%2FY5P7kguXN2QaOn6m5cdRytl97JJPdyelUXTEcWy7RHUeFJlhde%2Fx1ilfGJMT86WAKJIO6ef6paM72R7jYRyLA3uye0twXPm5qae2zjh3z18b6juvg%3D"}
+boxscoreParams={"view": "mBoxscore"}
 
 if (date.today().month < 9):
     last_year -= 1
@@ -16,10 +19,7 @@ data = dict()
 for year in range(start_year, last_year, 1):
     url = url = "https://fantasy.espn.com/apis/v3/games/ffl/leagueHistory/" + str(league_id) + "?seasonId=" + str(year)
 
-    r = requests.get(url,
-                     cookies={"swid": "{8095DE7B-520D-4619-BE38-33293C2BD1B4}",
-                              "espn_s2": "AEBiCbsmWjb1zWWG0%2B1DDj%2FxqAlV2eHcnYlKPJUA9f0WkcN9qmtyFeubqJDGH0EajqFWWjtUp5AWWtnBRhheukByJGEW0eJUfdHzlMMnNjplc7rXnXngO2DrT0P6r5sxJHOUmpvKz9wmbXDTBSWZkAHxZPtA1skkQMFGOqdxVt7GOkdV0xNj%2B5sdeeCQrKVS4sElWaIbhE13Piyqj%2FY5P7kguXN2QaOn6m5cdRytl97JJPdyelUXTEcWy7RHUeFJlhde%2Fx1ilfGJMT86WAKJIO6ef6paM72R7jYRyLA3uye0twXPm5qae2zjh3z18b6juvg%3D"},
-                     params={"view": "mBoxscore"})
+    r = requests.get(url, cookies=cookies, params=boxscoreParams)
 
     json_data = r.json()[0]
 
